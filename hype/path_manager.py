31edb5cd55b0d37c3a13222e9228e79dfffcbf7e
tsenoner <tobias.senoner94@gmail.com>
2025-11-19 15:51:47 +0100

    chore(cleanup): remove obsolete root-level scripts
    - Delete train_small.py, train_hierarchical.py
    - Remove analyze_hierarchy*.py files
    - Clean up build_transitive_closure.py and other root scripts
    - All functionality now in src/taxembed/ package

--- hype/path_manager.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from iopath.common.file_io import PathManager
-
-
-path_manager = PathManager()
--- tests/test_example.py
+++
@@ -1,6 +0,0 @@
-"""Example test module."""
-
-
-def test_placeholder():
-    """Placeholder test."""
-    assert True
